b300c7436de0a6deaa508599b5012f3e84730e3f
nbaiot <nbaiot@126.com>
2019-12-07 20:27:45 +0800

    add cmake module
    Signed-off-by: nbaiot <nbaiot@126.com>

---
+++ math/fftw.cc
@@ -0,0 +1,5 @@
+//
+// Created by shajia on 2019/12/7.
+//
+
+#include "fftw.h"
---
+++ math/fftw.h
@@ -0,0 +1,14 @@
+//
+// Created by shajia on 2019/12/7.
+//
+
+#ifndef NBSDK_FFTW_H
+#define NBSDK_FFTW_H
+
+
+class FFTW {
+
+};
+
+
+#endif //NBSDK_FFTW_H
